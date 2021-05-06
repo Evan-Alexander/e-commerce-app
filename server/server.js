adbff4d6cfb593e96e6b268d3072df7528ecf34a
jb <jasontbrown99@gmail.com>
2021-05-06 08:55:43 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+app.use(express.json())
+
+/// sanitize
+app.use(xss());
+app.use(mongoSanitize());
+
+
+
+
+
+const port = process.env.PORT || 3001
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+});
