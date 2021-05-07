48db3f8e9d11c4f05f1c9caed51a7691a7b63117
jb <jasontbrown99@gmail.com>
2021-05-07 13:01:34 -0700

    add delete and getAll routes

--- server/routes/brands.route.js
+++ server/routes/brands.route.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const brandController = require('../controllers/brand.controller');
 const auth = require('../middleware/auth');
 
+router.route('/brand/:id')
+  .get(brandController.getBrand)
+  .delete(auth('deleteAny', 'brand'), brandController.deleteBrand)
+
 router.post('/brand', auth('createAny', 'brand'), brandController.addBrand)
+router.get('/all', brandController.getBrands)
 
 module.exports = router;
--- server/services/brand.service.js
+++ server/services/brand.service.js
@@ -1,4 +1,7 @@
 const { Brand } = require('../models/brand');
+const { ApiError } = require('../middleware/apiError');
+const httpStatus = require('http-status');
+
 
 const addBrand = async (brandname) => {
   try {
@@ -12,6 +15,47 @@ const addBrand = async (brandname) => {
   }
 }
 
+const getBrandById = async (id) => {
+  try {
+    const brand = await Brand.findById(id);
+    if(!brand) throw new ApiError(httpStatus.NOT_FOUND, 'Brand not found');
+    return brand;
+  } catch (error) {
+    throw error;
+  }
+}
+
+const deleteBrandById = async (id) => {
+  try {
+    const brand = await Brand.findByIdAndRemove(id);
+    return brand;
+  } catch (error) {
+    throw error;
+  }
+}
+
+const getBrands = async (args) => {
+  try {
+    // set defaults
+    let order = args.order ? args.order : "asc";
+    let limit = args.limit ? args.limit : 5;
+
+    const brands = await Brand
+    .find({})
+    .sort([
+      ["_id", order]
+    ])
+    .limit(limit)
+    if(!brands) throw new ApiError(httpStatus.NOT_FOUND, 'No brands found.');
+    return brands;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
-  addBrand
+  addBrand,
+  getBrandById,
+  deleteBrandById,
+  getBrands
 }
