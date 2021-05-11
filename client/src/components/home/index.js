83ced7ac2a6f9dde3a617999e34e5ccb658d9dfc
jb <jasontbrown99@gmail.com>
2021-05-11 13:56:03 -0700

    add promotional reusable comp

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import FeaturedSlider from "./FeaturedSlider";
+import SlimPromotion from "../utils/SlimPromotion";
 
+const slimPromotion = {
+  img: "/images/featured/featured_home_3.jpg",
+  lineOne: "Up to 40% off",
+  lineTwo: "For second hand guitars",
+  lineTitle: "Show now",
+  linkTo: "/shop",
+};
 const Home = () => {
   return (
     <>
       <FeaturedSlider />
+      <SlimPromotion items={slimPromotion} />
     </>
   );
 };
