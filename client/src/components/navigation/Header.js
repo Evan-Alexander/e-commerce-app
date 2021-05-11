583030bf6be2a49dc95e9ce76842924f2c4c5403
jb <jasontbrown99@gmail.com>
2021-05-11 06:36:30 -0700

    initial client setup w footer n header

---
+++ client/src/components/navigation/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="bck_b_light">
+      <div className="container">
+        <div className="left">
+          <div className="logo">WAVES</div>
+        </div>
+        <div className="right">
+          <div className="top">
+            <>
+              <div className="cart_link">
+                <span>0</span>
+                <Link to="dashboard/user/user_cart">My Cart</Link>
+              </div>
+              <Link to="/dashboard">My account</Link>
+              <span onClick={() => alert("log out")}>Log out</span>
+              <Link to="/sign_in">Log in</Link>
+            </>
+          </div>
+          <div className="bottom">
+            <Link to="/">Home</Link>
+            <Link to="/dashboard">Shop</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
